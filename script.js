d5474b9c77530135b89778c031722194339fbdd2
James Phillip Mayor <117265110+JpMayor1@users.noreply.github.com>
2022-12-31 00:47:28 +0800

    1st Commit
    Digital Clock with Glow Effects

---
+++ script.js
@@ -0,0 +1,75 @@
+function updateClock() {
+    var now = new Date();
+    var dname = now.getDay();
+    var mo = now.getMonth();
+    var dnum = now.getDate();
+    var yr = now.getFullYear();
+    var hou = now.getHours();
+    var min = now.getMinutes();
+    var sec = now.getSeconds();
+    var pe = "AM";
+
+    if (hou == 0) {
+        hou = 12;
+    }
+    if (hou > 12) {
+        hou = hou - 12;
+        pe = "PM";
+    }
+
+    Number.prototype.pad = function (digits) {
+        for (var n = this.toString(); n.length < digits; n = 0 + n);
+        return n;
+    };
+
+    var months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    var week = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday ",
+    ];
+    var ids = [
+        "dayname",
+        "month",
+        "daynum",
+        "year",
+        "hour",
+        "minutes",
+        "seconds",
+        "period",
+    ];
+    var values = [
+        week[dname],
+        months[mo],
+        dnum.pad(2),
+        yr,
+        hou.pad(2),
+        min,
+        sec.pad(2),
+        pe,
+    ];
+    for (var i = 0; i < ids.length; i++)
+        document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+}
+
+function initClock() {
+    updateClock();
+    window.setInterval("updateClock()", 1);
+}
